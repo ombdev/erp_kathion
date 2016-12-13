a6b23fb85b333fef357b0b33251b7b6a7c17b624
j2eeserver <j2eeserver@server01>
2016-12-13 08:42:01 -0500

    Patch to deal with encription of RSA SHA1 for 2048 bits
    Signed-off-by: j2eeserver <j2eeserver@server01>

--- erp-quimicos/src/main/java/com/agnux/cfd/v2/CryptoEngine.java
+++ erp-quimicos/src/main/java/com/agnux/cfd/v2/CryptoEngine.java
@@ -1,13 +1,14 @@
 package com.agnux.cfd.v2;
 
-
+import java.io.DataInputStream;
+import java.io.File;
 import java.security.*;
 import javax.security.cert.X509Certificate;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import org.apache.commons.ssl.PKCS8Key;
-
 
 /**
  *
@@ -15,12 +16,13 @@ import org.apache.commons.ssl.PKCS8Key;
  */
 public class CryptoEngine {
 
-/*********************** INICIA - Codigo para la md5sum ****************************/
-   
-   private static byte[] createChecksum(String filename){
-       byte[] arreglo_retorno;
-       try{
-            InputStream fis =  new FileInputStream(filename);
+    /**
+     * ********************* INICIA - Codigo para la md5sum ***************************
+     */
+    /*private static byte[] createChecksum(String filename) {
+        byte[] arreglo_retorno;
+        try {
+            InputStream fis = new FileInputStream(filename);
 
             byte[] buffer = new byte[1024];
             MessageDigest complete = MessageDigest.getInstance("MD5");
@@ -33,46 +35,47 @@ public class CryptoEngine {
             } while (numRead != -1);
             fis.close();
             arreglo_retorno = complete.digest();
-       }
-       catch( Exception e){
+        } catch (Exception e) {
             System.out.println(e.toString());
             arreglo_retorno = null;
-       }
+        }
 
-       return arreglo_retorno;
-   }
+        return arreglo_retorno;
+    }*/
 
-   /**Convierte una fichero a una cadena md5
-   * @param filename
-   * @return la cadena md5.
-   */
-   public static String md5sum(String filename) throws Exception {
-     byte[] b = createChecksum(filename);
-     String result = "";
-     for (int i=0; i < b.length; i++) {
-       result +=
-          Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
-      }
-     return result;
-   }
-/*********************** FINALIZA - Codigo para la md5sum **************************/
+    /**
+     * Convierte una fichero a una cadena md5
+     *
+     * @param filename
+     * @return la cadena md5.
+     */
+    /*public static String md5sum(String filename) throws Exception {
+        byte[] b = createChecksum(filename);
+        String result = "";
+        for (int i = 0; i < b.length; i++) {
+            result
+                    += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+        }
+        return result;
+    }*/
 
+    /**
+     * ********************* FINALIZA - Codigo para la md5sum *************************
+     */
 
-/*********************** INICIA - Codigo para la sign ********************************/
-
-   /**
-    * Verify a MD5 Hash Signed.
-    *
-    * @param pk
-    *            The public key from The Cert used.
-    * @param cadenaoriginal
-    *            The Origina String unsigned.
-    * @param hash_md5_firmado
-    *            The Md5 Signed on Array byte format.
-    *
-    * @return True if the verify was successful.
-    */
-    public static boolean verifySign(PublicKey pk , String cadenaoriginal, byte[] hash_md5_firmado){
+    /**
+     * ********************* INICIA - Codigo para la sign *******************************
+     */
+    /**
+     * Verify a MD5 Hash Signed.
+     *
+     * @param pk The public key from The Cert used.
+     * @param cadenaoriginal The Origina String unsigned.
+     * @param hash_md5_firmado The Md5 Signed on Array byte format.
+     *
+     * @return True if the verify was successful.
+     */
+    /*   public static boolean verifySign(PublicKey pk , String cadenaoriginal, byte[] hash_md5_firmado){
         boolean valor_retorno = false;
 
         Signature firma;
@@ -87,18 +90,15 @@ public class CryptoEngine {
 
 
         return valor_retorno;
-    }
-
-
-   /**
-    * Extract a Public key from a CertFile.
-    *
-    * @param certfile
-    *            The file with a Path where private key is found.
-    *
-    * @return A public key instance.
-    */
-    public static PublicKey getPublicKeyFromCertFile(String certifle){
+    }*/
+    /**
+     * Extract a Public key from a CertFile.
+     *
+     * @param certfile The file with a Path where private key is found.
+     *
+     * @return A public key instance.
+     */
+    /*    public static PublicKey getPublicKeyFromCertFile(String certifle){
         PublicKey pk = null;
         InputStream fis;
         try {
@@ -110,58 +110,51 @@ public class CryptoEngine {
         }
 
         return pk;
-    }
-
-
-   /**
-    * Digitally Sign a String inserted.
-    *
-    * @param filename
-    *            The file with a Path where private key is found.
-    * @param password
-    *            The password of The Prtivate key.
-    * @param cadenaoriginal
-    *            The String to be Signed.
-    *
-    * @return A character array with the Base64 encoded data.
-    */
-    public static String sign(String filename, String password , String cadenaoriginal) {
+    }*/
+    /**
+     * Digitally Sign a String inserted.
+     *
+     * @param filename The file with a Path where private key is found.
+     * @param password The password of The Prtivate key.
+     * @param cadenaoriginal The String to be Signed.
+     *
+     * @return A character array with the Base64 encoded data.
+     */
+    public static String sign(String filename, String password, String cadenaoriginal) {
 
         String valor_retorno = null;
-        InputStream archivoClavePrivada = null;
+        //InputStream archivoClavePrivada = null;
         byte[] clavePrivada = null;
         Signature firma = null;
         PKCS8Key pkcs8 = null;
         PrivateKey pk = null;
 
         try {
-            archivoClavePrivada =  new FileInputStream(filename);
-            clavePrivada = getBytesFromInputStream(archivoClavePrivada);
-        }
-        catch( Exception e){
+            clavePrivada = serializeFile(filename);
+            //archivoClavePrivada = new FileInputStream(filename);
+            //clavePrivada = getBytesFromInputStream(archivoClavePrivada);
+        } catch (Exception e) {
             System.out.println(e.toString());
         }
-	
-        
+
         try {
             pkcs8 = new PKCS8Key(clavePrivada, password.toCharArray());
             pk = pkcs8.getPrivateKey();
             firma = Signature.getInstance("SHA1withRSA", "SunRsaSign");
             firma.initSign(pk);
-        }
-        catch(GeneralSecurityException e){
+        } catch (GeneralSecurityException e) {
             e.printStackTrace();
         }
-	
-	try {
-		firma.update(cadenaoriginal.getBytes("UTF-8"));
-		byte[] firmaDigital = firma.sign();
-		valor_retorno = new String(Base64Coder.encode(firmaDigital));
-		//valor_retorno = base64(new String(firma.sign()));
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-        
+
+        try {
+            firma.update(cadenaoriginal.getBytes("UTF-8"));
+            byte[] firmaDigital = firma.sign();
+            valor_retorno = new String(Base64Coder.encode(firmaDigital));
+            //valor_retorno = base64(new String(firma.sign()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return valor_retorno;
     }
 
@@ -181,11 +174,11 @@ public class CryptoEngine {
         return new String(psB64Certificate);
     }
 
-    /*
+ /*
      * Agregado Viernes 9 de julio del 2010
      *
      */
-    public static X509Certificate decodeCertFromBase64(String psB64Certificate){
+ /*    public static X509Certificate decodeCertFromBase64(String psB64Certificate){
         X509Certificate cert = null;
         try {
             cert = X509Certificate.getInstance(Base64Coder.decode(psB64Certificate));
@@ -195,16 +188,15 @@ public class CryptoEngine {
 
 
         return cert;
-    }
+    }*/
 
-    public static PublicKey getPublicKeyFromCertOnBase64(String psB64Certificate){
+ /*    public static PublicKey getPublicKeyFromCertOnBase64(String psB64Certificate){
         X509Certificate cert = decodeCertFromBase64(psB64Certificate);
         return cert.getPublicKey();
-    }
+    }*/
 
-    /**/
-
-    private static byte[] getBytesFromInputStream(InputStream is) {
+ 
+/*    private static byte[] getBytesFromInputStream(InputStream is) {
         int totalBytes = 714;
         byte[] buffer = null;
         try {
@@ -215,8 +207,17 @@ public class CryptoEngine {
             e.printStackTrace();
         }
         return buffer;
+    }*/
+    private static byte[] serializeFile(final String absPathToFile) throws FileNotFoundException, IOException {
+        File f = new File(absPathToFile);
+        FileInputStream fis = new FileInputStream(f);
+        DataInputStream dis = new DataInputStream(fis);
+        byte[] buffer = new byte[(int) f.length()];
+        dis.readFully(buffer);
+        dis.close();
+        return buffer;
     }
-
-/*********************** FINALIZA - Codigo para la sign ****************************/
-
+    /**
+     * ********************* FINALIZA - Codigo para la sign ***************************
+     */
 }
