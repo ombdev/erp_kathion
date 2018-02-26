562dfea6db400d3277b5dcd32d38a212a0a429f2
Bob Ross <pianodaemon@gmail.com>
2018-02-25 22:50:27 -0600

    web-ui: modification for uso and forma cfdi into gui

--- erp-quimicos/src/main/java/com/agnux/kemikal/controllers/PotCatCusorder.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/controllers/PotCatCusorder.java
@@ -18,7 +18,9 @@ public class PotCatCusorder {
     public String warehouse_id;
     public String currency_id;
     public String sup_credays_id;
+    public String forma_pago_id;
     public String met_pago_id;
+    public String uso_id;
     public String pedido_id;
     public String tasaretimmex;
     public String currency_val;
@@ -48,6 +50,8 @@ public class PotCatCusorder {
                 + currency_id + ","
                 + sup_credays_id + ","
                 + met_pago_id + ","
+                + forma_pago_id + ","
+                + uso_id + ","
                 + pedido_id + ","
                 + tasaretimmex + ","
                 + currency_val + ","
