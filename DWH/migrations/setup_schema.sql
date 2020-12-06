8a2eaec4852d0da7063cf26edb2854183cdf7876
Edwin Plauchu <eplauchu@commscope.com>
2020-12-05 19:22:25 -0600

    better schema creation

--- DWH/migrations/setup_schema.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS data_mars_sales;
-CREATE DATABASE data_mars_sales;
--- DWH/scripts/create_schema.sh
+++ DWH/scripts/create_schema.sh
@@ -5,7 +5,8 @@ echo "Creating database..."
 CONTAINER="rdbms_staging"
 export MYSQL_PWD=$MYSQL_ROOT_PASSWORD
 
-mysql $MYSQL_DATABASE -u root < /migrations/setup_schema.sql
+
+echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql -u root -h 127.0.0.1
 
 # Creates the time dimension along with its data
 mysql $MYSQL_DATABASE -u root < /migrations/dim_time.sql
