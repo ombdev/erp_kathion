8b536a0ea4e91ce9a074d4caf2198e4fe5e2c3f2
Edwin Plauchu <eplauchu@commscope.com>
2020-12-10 10:17:54 -0600

    Server part of example of protobuff in container
    Signed-off-by: Edwin Plauchu <eplauchu@commscope.com>

--- DOS/sales-api/service/run.py
+++ DOS/sales-api/service/run.py
@@ -14,7 +14,7 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
 def _engage():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('[::]:10090')
     server.start()
     server.wait_for_termination()
 
