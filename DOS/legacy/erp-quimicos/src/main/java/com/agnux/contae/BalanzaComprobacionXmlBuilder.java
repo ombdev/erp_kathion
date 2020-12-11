76620560401ff1ebed06d8ce341cdb5cf30fa8f6
Edwin Plauchu <eplauchu@commscope.com>
2020-12-11 16:52:26 -0600

    removed futile code

--- DOS/legacy/erp-quimicos/src/main/java/com/agnux/contae/BalanzaComprobacionXmlBuilder.java
+++ DOS/legacy/erp-quimicos/src/main/java/com/agnux/contae/BalanzaComprobacionXmlBuilder.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.agnux.contae;
 
 import java.io.ByteArrayOutputStream;
@@ -17,13 +13,7 @@ import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 import mx.gob.sat.esquemas.contabilidade._1_1.balanzacomprobacion.Balanza;
 
-/**
- *
- * @author Noe Martinez
- * gpmarsan@gmail.com
- * 22/abril/2016
- * 
- */
+
 public class BalanzaComprobacionXmlBuilder {
     private ByteArrayOutputStream baos = new ByteArrayOutputStream();
     
