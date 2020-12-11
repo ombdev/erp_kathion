b0c0c982f86107dc88acd0086390bf7c3e8e4ed2
Edwin Plauchu <eplauchu@commscope.com>
2020-12-11 16:31:18 -0600

    Removed needless code

--- DOS/legacy/erp-quimicos/src/main/java/com/agnux/kemikal/controllers/ComFacComprasController.java
+++ DOS/legacy/erp-quimicos/src/main/java/com/agnux/kemikal/controllers/ComFacComprasController.java
@@ -1,8 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
-*/
 package com.agnux.kemikal.controllers;
+
 
 import com.agnux.cfd.v2.Base64Coder;
 import com.agnux.common.helpers.FileHelper;
@@ -37,11 +34,7 @@ import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- *
- * @author Noe Martinez
- * gpmarsan@gmail.com
- */
+
 @Controller
 @SessionAttributes({"user"})
 @RequestMapping("/entradamercancias/")
--- DOS/legacy/erp-quimicos/src/main/java/com/agnux/kemikal/controllers/InvOrdenTrasController.java
+++ DOS/legacy/erp-quimicos/src/main/java/com/agnux/kemikal/controllers/InvOrdenTrasController.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.agnux.kemikal.controllers;
 
 import com.agnux.cfd.v2.Base64Coder;
@@ -44,13 +40,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-/**
- *
- * @author Noe Martinez
- * gpmarsan@gmail.com
- * 01/noviembre/2012
- * 
- */
+
 @Controller
 @SessionAttributes({"user"})
 @RequestMapping("/invordentras/")
