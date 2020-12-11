3560753906089e228f13f57f4ffc2d1afac0572f
Edwin Plauchu <eplauchu@commscope.com>
2020-12-11 16:59:50 -0600

    Removed futile stuff

--- DOS/legacy/erp-quimicos/src/main/java/com/agnux/kemikal/controllers/ProductosController.java
+++ DOS/legacy/erp-quimicos/src/main/java/com/agnux/kemikal/controllers/ProductosController.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.agnux.kemikal.controllers;
 
 import com.agnux.cfd.v2.Base64Coder;
@@ -34,12 +30,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 
-
-/**
- *
- * @author gpmarsan@gmail.com
- * Noe Martinez
- */
 @Controller
 @SessionAttributes({"user"})
 @RequestMapping("/productos/")
