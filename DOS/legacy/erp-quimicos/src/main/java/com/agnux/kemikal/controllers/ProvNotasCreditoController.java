c20f174db1ad2164dc9917507409b75895a57a05
Edwin Plauchu <eplauchu@commscope.com>
2020-12-12 08:38:43 -0600

    remove futile stuff

--- DOS/legacy/erp-quimicos/src/main/java/com/agnux/kemikal/controllers/ProvNotasCreditoController.java
+++ DOS/legacy/erp-quimicos/src/main/java/com/agnux/kemikal/controllers/ProvNotasCreditoController.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.agnux.kemikal.controllers;
 
 import com.agnux.cfd.v2.Base64Coder;
@@ -42,11 +38,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-/**
- * @author Noe Martinez
- * gpmarsan@gmail.com
- * 02/octubre/2012
- */
+
 @Controller
 @SessionAttributes({"user"})
 @RequestMapping("/provnotascredito/")
